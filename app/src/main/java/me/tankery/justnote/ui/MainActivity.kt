bd98813b65ec7ca09c1c550ba5c15157ede07bfb
Tankery Chen <tankery.chen@gmail.com>
2019-08-01 16:33:03 +0800

    Setup using Jetpack navigation component

--- app/src/main/java/me/tankery/justnote/ui/MainActivity.kt
+++ app/src/main/java/me/tankery/justnote/ui/MainActivity.kt
@@ -2,15 +2,49 @@ package me.tankery.justnote.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
-import me.tankery.justnote.ui.list.MainNotesFragment
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import me.tankery.justnote.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportFragmentManager.commit {
-            replace(android.R.id.content, MainNotesFragment())
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        navController = findNavController(R.id.nav_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        // Set up ActionBar
+        setSupportActionBar(findViewById(R.id.toolbar))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Set up navigation menu
+        findViewById<NavigationView>(R.id.navigation_view).setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 }
